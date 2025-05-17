0e44a42a763cd0ec46b6333b13ac8e2511f89388
Franco Castro <francogabrielcastro15@gmail.com>
2025-05-16 21:05:37 -0300

    Cambios

--- Assets/Scripts/LimitPlayer.cs
+++ Assets/Scripts/LimitPlayer.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class LimitPlayer: MonoBehaviour
+public class LimitPlayer : MonoBehaviour
 {
-
-
-    // Variables que utilizaremos para fijar limites
-    [SerializeField] private float leftLimit;
-    [SerializeField] private float rightLimit;
-    [SerializeField] private float bottomLimit;
-    [SerializeField] private float topLimit;
+    // Variables que utilizaremos para fijar límites (valores por defecto incluidos)
+    [SerializeField] private float leftLimit = -7.2f;
+    [SerializeField] private float rightLimit = 7.32f;
+    [SerializeField] private float bottomLimit = -3.4f;
+    [SerializeField] private float topLimit = 2.77f;
 
     private void FixedUpdate()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftLimit, rightLimit), Mathf.Clamp(transform.position.y, bottomLimit, topLimit), transform.position.z);
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+        );
     }
 }
